b96223c12e059dcdbf14117c044db69d158e6a59
sme-upge <upge@sme.prefeitura.sp.gov.br>
2026-01-27 09:37:55 -0300

    Update atualiza_base.R

--- orcamento/atualiza_base.R
+++ orcamento/atualiza_base.R
@@ -12,6 +12,7 @@ get_url <- function(data) {
                 ano, "/basedadosexecucaoconsolidados_", mes_ano, ".xlsx"))
 }
 
+# Tenta o mês atual, se falhar tenta o anterior
 url_base <- get_url(Sys.Date())
 arquivo_temp <- tempfile(fileext = ".xlsx")
 
@@ -43,3 +44,42 @@ mapa_unidade <- c(
   "Diretoria Regional de Educação - Capela do Socorro" = "Capela do Socorro",
   "Diretoria Regional de Educação Penha" = "Penha",
   "Diretoria Regional de Educação - Penha" = "Penha",
+  "Diretoria Regional de Educação Santo Amaro" = "Santo Amaro",
+  "Diretoria Regional de Educação - Santo Amaro" = "Santo Amaro",
+  "Diretoria Regional de Educação Itaquera" = "Itaquera",
+  "Diretoria Regional de Educação - Itaquera" = "Itaquera",
+  "Diretoria Regional de Educação São Miguel" = "São Miguel",
+  "Diretoria Regional de Educação - São Miguel" = "São Miguel",
+  "Diretoria Regional de Educação Guaianases" = "Guaianases",
+  "Diretoria Regional de Educação - Guaianases" = "Guaianases",
+  "Diretoria Regional de Educação Butantã" = "Butantã",
+  "Diretoria Regional de Educação - Butantã" = "Butantã",
+  "Diretoria Regional de Educação São Mateus" = "São Mateus",
+  "Diretoria Regional de Educação - São Mateus" = "São Mateus",
+  "Coordenadoria de Alimentação Escolar" = "Coordenadoria de Alimentação Escolar",
+  "Departamento da Merenda Escolar" = "Coordenadoria de Alimentação Escolar",
+  "Departamento de Alimentação Escolar" = "Coordenadoria de Alimentação Escolar"
+)
+
+# Filtro e Limpeza
+base_filtrada <- base_completa %>%
+  mutate(
+    Cd_AnoExecucao = as.numeric(Cd_AnoExecucao),
+    Ds_Orgao = trimws(Ds_Orgao),
+    DataInicial = as.Date(DataInicial),
+    DataFinal = as.Date(DataFinal),
+    DataExtracao = Sys.time()
+  ) %>%
+  filter(Cd_AnoExecucao >= 2010, Sigla_Orgao == "SME")
+
+# Aplicar o mapa de unidades
+base_filtrada$Ds_Unidade <- recode(base_filtrada$Ds_Unidade, !!!mapa_unidade)
+
+# 3. SALVAR RESULTADO
+# Criar pasta orcamento se não existir
+if(!dir.exists("orcamento")) { dir.create("orcamento") }
+
+# Gravar o CSV
+write_csv(base_filtrada, "orcamento/Execucao_Orcamentaria_Atualizada.csv")
+
+message("Arquivo CSV gerado com sucesso!")
